b45b2b951eee8edb2245b169dff1d032391ee323
MdIbrahimKholil7 <kholilmdibrahim17@gmail.com>
2022-08-30 00:50:44 +0600

    firebase add

--- src/_firebase_init.js
+++ src/_firebase_init.js
@@ -1,18 +1,41 @@
+// // Import the functions you need from the SDKs you need
+// import { initializeApp } from "firebase/app";
+// import {getAuth} from 'firebase/auth'
+// // TODO: Add SDKs for Firebase products that you want to use
+// // https://firebase.google.com/docs/web/setup#available-libraries
+
+// // Your web app's Firebase configuration
+// const firebaseConfig = {
+//   apiKey:process.env.REACT_APP_Api_Key,
+//   authDomain:process.env.REACT_APP_Auth_Domain,
+//   projectId:process.env.REACT_APP_Project_Id,
+//   storageBucket:process.env.REACT_APP_Storage_Bucket,
+//   messagingSenderId:process.env.REACT_APP_Messaging_SenderId,
+//   appId:process.env.REACT_APP_App_Id,
+// };
+
+
+
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import {getAuth} from 'firebase/auth'
+import { getAuth } from "firebase/auth";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey:process.env.REACT_APP_Api_Key,
-  authDomain:process.env.REACT_APP_Auth_Domain,
-  projectId:process.env.REACT_APP_Project_Id,
-  storageBucket:process.env.REACT_APP_Storage_Bucket,
-  messagingSenderId:process.env.REACT_APP_Messaging_SenderId,
-  appId:process.env.REACT_APP_App_Id,
+  apiKey: "AIzaSyA_utZx_8G9O61x_xus09FAG-Uz6BcQSMQ",
+  authDomain: "doctor-portals-2.firebaseapp.com",
+  projectId: "doctor-portals-2",
+  storageBucket: "doctor-portals-2.appspot.com",
+  messagingSenderId: "590752152200",
+  appId: "1:590752152200:web:60eec61c9f2b610c64fd3d"
 };
+
+
+
+
+
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
